7383fa004cae87f533410efbfbf312cdad6670d4
HarcourtsCentralBSD <lyonel.nelson.ln@gmail.com>
2025-07-11 22:34:56 +0700

    Add files via upload

--- app.js
+++ app.js
@@ -3,16 +3,17 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/11.10.0/fireba
 import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.10.0/firebase-analytics.js";
 import { getFirestore, collection, addDoc } from "https://www.gstatic.com/firebasejs/11.10.0/firebase-firestore.js";
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-    measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
+    apiKey: "AIzaSyCldfDsgTCDt0By2Yev0zrY730ck235I18",
+    authDomain: "rekap-penjualan-f7b06.firebaseapp.com",
+    projectId: "rekap-penjualan-f7b06",
+    storageBucket: "rekap-penjualan-f7b06.firebasestorage.app",
+    messagingSenderId: "533820584568",
+    appId: "1:533820584568:web:38f6ede9b66df24737738d",
+    measurementId: "G-J5XT2V7YFN"
 };
-    
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
--- search.js
+++ search.js
@@ -4,15 +4,14 @@ import { getFirestore, collection, onSnapshot, query, doc, deleteDoc } from "htt
 
 // Your web app's Firebase configuration (copy from app.js)
 const firebaseConfig = {
-        apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-        authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-        projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-        storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-        messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-        appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-        measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
-    };
-    
+    apiKey: "AIzaSyCldfDsgTCDt0By2Yev0zrY730ck235I18",
+    authDomain: "rekap-penjualan-f7b06.firebaseapp.com",
+    projectId: "rekap-penjualan-f7b06",
+    storageBucket: "rekap-penjualan-f7b06.firebasestorage.app",
+    messagingSenderId: "533820584568",
+    appId: "1:533820584568:web:38f6ede9b66df24737738d",
+    measurementId: "G-J5XT2V7YFN"
+};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
